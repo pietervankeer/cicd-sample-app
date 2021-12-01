aa1012148ca3ef0b97c7f3596ac87b0969365fbd
Pieter Van Keer <60261376+pietervankeer@users.noreply.github.com>
2021-12-01 14:13:04 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+if [ ! -e tempdir ]; then
+  mkdir tempdir
+  mkdir tempdir/templates
+  mkdir tempdir/static
+fi
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
